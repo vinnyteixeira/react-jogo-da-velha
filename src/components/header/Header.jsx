08b14bdd6cd41206018b6afd7d38c249cbe66f3e
Vinicius Teixeira <vinyteixeira.12@gmail.com>
2022-10-29 14:00:17 -0300

    Criado componente de icone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,17 @@
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
 
 import Title from "../title/Title";
 import Subtitle from "../subtitle/Subtitle";
-
+import Icon from "../icon/Icon";
 
 function Header() {
   return (
     <div className={styles.header}>
       <Title>Jogo da velha</Title>
       <Subtitle>Criado por Vinícius Teixeira</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link="https://github.com/vinnyteixeira"/>
+      </div>
     </div>
   );
 }
