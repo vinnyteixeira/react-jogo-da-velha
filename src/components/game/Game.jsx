a0bcd5cd39f73f0dc24c7b0981c000688207936e
Vinicius Teixeira <vinyteixeira.12@gmail.com>
2022-10-31 14:39:49 -0300

    criado componente game

---
+++ src/components/game/Game.jsx
@@ -0,0 +1,20 @@
+import styles from "./Game.module.css"
+import GameOption from "../gameOption/Gameoption"
+
+function Game () {
+    return (
+        <div className={styles.game}>   
+            <GameOption />
+            <GameOption />
+            <GameOption />
+            <GameOption />
+            <GameOption />
+            <GameOption />
+            <GameOption />
+            <GameOption />
+            <GameOption />
+        </div>
+    )
+}
+
+export default Game
---
+++ src/components/gameOption/GameOption.jsx
@@ -0,0 +1,11 @@
+import styles from "./GameOption.module.css"
+
+function GameOption () {
+    return (
+        <div className={styles.gameOption}>
+            
+        </div>
+    )
+}
+
+export default GameOption
