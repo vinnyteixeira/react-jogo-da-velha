07be45ea90db075cd60e6d79495422c7757a4651
Vinicius Teixeira <vinyteixeira.12@gmail.com>
2022-11-02 13:45:55 -0300

    Corrigido responsividade

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,7 +3,7 @@ import styles from "./Button.module.css"
 function Button ({children, onClick, disabled}) {
     return (
         <button
-        className={styles.Button} 
+        className={styles.button} 
         onClick={onClick}
         disabled={disabled}
         >
